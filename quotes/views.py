067554559a08401dbb579c48781badb749d22245
Vladimir <tchaikovski@mail.ru>
2021-02-15 17:39:11 +0300

    Quotes Final

--- quotes/views.py
+++ quotes/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
-# Create your views here.
+from .models import Quote
+from .forms import QuoteForm
+from pages.models import Page
+
+
+def quote_req(request):
+    submitted = False
+    if request.method == 'POST':
+        form = QuoteForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/quote/?submitted=True')
+    else:
+        form = QuoteForm()
+        if 'submitted' in request.GET:
+            submitted = True
+    return render(request, 'quotes/quote.html',
+                  {'form': form, 'page_list': Page.objects.all(), 'submitted': submitted})
+
