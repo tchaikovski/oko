ce19cd0e5b6fca4f027ad1bca0e0d66f7f81a801
Vladimir <tchaikovski@mail.ru>
2021-02-15 13:36:16 +0300

    Создаем quotes

--- pages/views.py
+++ pages/views.py
@@ -29,6 +29,16 @@ def contact(request):
         form = ContactForm(request.POST)
         if form.is_valid():
             cd = form.changed_data
+            # Ниже форма отправки сообщения на почту
+            # assert False
+            # con = get_connection('django.core.mail.backends.console.EmailBackend')
+            # send_mail(
+            # cd['subject'],
+            # cd['message'],
+            # cd.get('email', 'noreply@example.com'),
+            # ['siteowner@example.com'],
+            # connection = con
+            # )
             return HttpResponseRedirect('/contact?submitted=True')
     else:
         form = ContactForm()
