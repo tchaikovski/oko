c3029795afa8325f2b790322fd171fcd03754483
Vladimir <tchaikovski@mail.ru>
2021-02-16 21:16:48 +0300

    Регистрация работает, нужно определится с полями

--- okonew/urls.py
+++ okonew/urls.py
@@ -15,9 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import TemplateView
+from quotes.views import Register
+
+
 
 urlpatterns = [
+    path('register/success', TemplateView.as_view(template_name='registration/success.html'), name='register-success'),
+    path('register/', Register.as_view(), name='register'),
     path('admin/', admin.site.urls),
     path('quote/', include('quotes.urls')),
+    path('', include('django.contrib.auth.urls')),
     path('', include('pages.urls')),
 ]
