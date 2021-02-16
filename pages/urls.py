7832331ce21e41c559ebe1f371af8c31d370c112
Vladimir <tchaikovski@mail.ru>
2021-02-16 13:04:49 +0300

    todo по главной странице

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+#   TODO: убрать гравную страницу
 urlpatterns = [
     path('', views.index, name='home'),
     path('contact', views.contact, name='contact'),
